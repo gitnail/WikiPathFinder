2c60481c42e027b041e23192081c774506664828
gitnail <n.nail15@mail.ru>
2018-01-14 01:30:41 +0300

    Introduce WikiFindPath

---
+++ main.py
@@ -0,0 +1,103 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
+import requests
+from lxml.html import fromstring
+from lxml import html
+
+class Queue:
+    def __init__(self):
+        self.a = []
+        self.r = 0
+        self.l = 0
+    def size(self):
+        return self.r - self.l
+    def pop(self):
+        self.l += 1
+    def push(self, x):
+        if (self.r == len(self.a)):
+            self.a.append(x)
+        else:
+            self.a[self.r] = x
+        self.r += 1
+    def front(self):
+        return self.a[self.l]
+    def empty(self):
+        return self.size() == 0
+
+pref = ""
+
+def getLinks(url):
+    try:
+        page = requests.get(url)
+        s = page.text
+        i = 0
+        res = []
+        while i + 5 < len(s):
+            if s[i] + s[i + 1] + s[i + 2] + s[i + 3] + s[i + 4] + s[i + 5] == "a href":
+                found = False
+                b = 0
+                t = ""
+                while s[i] + s[i + 1] + s[i + 2] + s[i + 3] != "</a>":
+                    if s[i] + s[i + 1] + s[i + 2] + s[i + 3] + s[i + 4] + s[i + 5] == "title=":
+                        i += 7;
+                        found = True
+                        while s[i] != '"':
+                            t += s[i]
+                            i += 1
+                    i += 1 
+                if found:
+                    if not (";" in t or ":" in t):
+                        link = t.replace(' ', '_')
+                        res.append([pref + link, t])
+            i += 1
+        return res
+    except:
+        return []
+
+def getTitle(url):
+    try:
+        r = requests.get(url)
+        tree = fromstring(r.content)
+        s = tree.findtext('.//title')
+        return s[:-12]
+    except:
+        return "noname"
+
+if __name__ == '__main__':
+    start = input("Enter start url\n")
+    tmp = start
+    if tmp[len(tmp) - 1] == '/':
+        tmp = tmp[:len(tmp) - 2]
+    while tmp[len(tmp) - 1] != '/':
+        tmp = tmp[:-1]
+    pref = tmp
+    end = input("Enter end url\n")
+    end_t = getTitle(end)
+    print("Going from " + '"' + getTitle(start) + '"' + " to " '"' + end_t + '"')
+    q = Queue()
+    q.push([start, getTitle(start)])
+    d = {}
+    d[getTitle(start)] = 0
+    while not q.empty():
+        v = q.front()[0]
+        v_t = q.front()[1]
+        if v == end:
+            print("Done with " + str(d[v_t]) + " steps")
+            exit(0)
+        q.pop()
+        links = getLinks(v)
+        print(v_t)
+        print("current deep = " + str(d[v_t]))
+        for link, link_t in links:
+            if link_t not in d:
+                d[link_t] = d[v_t] + 1
+                if link_t == end_t:
+                    print("Done with " + str(d[link_t]) + " steps")
+                    exit(0)
+                q.push([link, link_t])
+print("Can't get end link")
+
+
+
+
